5ae63730e0203f333c586963e3bea7b793865f90
Elijah Desktop <56444910+EaStory@users.noreply.github.com>
2020-12-09 18:27:10 -0500

    added comments to the controller

--- controller-input/controller-input.ino
+++ controller-input/controller-input.ino
@@ -1,3 +1,15 @@
+//Elijah Story
+//12-9-2020
+//NMD211
+
+/*
+ * 90% of the code was taken from the plotter file in the Adafruit MPU6050 tab in the examples.
+ * The code I needed was striped from the plotter. The code takes the acceleration values and adjusts 
+ * them based on a manual zeroing process. The values I used may not be the best for all devices. Once the data is zeroed, 
+ * it is sent to the Serial with a tab separating the two values. Once the last value is sent, 
+ * it adds a new line character to the Serial. This lets Processing know that both the X and Y values are done being sent. 
+ */
+
 #include <Adafruit_MPU6050.h>
 
 Adafruit_MPU6050 mpu;
@@ -15,21 +27,21 @@ void setup(void) {
     }
   }
   mpu_accel = mpu.getAccelerometerSensor();
-  Serial.println('s');
+  Serial.println('s');      //tells processing it is ready
 }
 
 void loop() {
   sensors_event_t accel;
   mpu_accel->getEvent(&accel);
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0){              //if processing has asked for data
 
-    accel.acceleration.x -= 0.52;
-    accel.acceleration.y += 0.0;
+    accel.acceleration.x -= 0.52;           //zero the X value
+    accel.acceleration.y += 0.0;            //zero the Y value
 
-    char input = Serial.read();
+    char input = Serial.read();             //throw away processings request signal 
 
-    Serial.print(accel.acceleration.x,2);
-    Serial.print('\t');
-    Serial.println(accel.acceleration.y,2);
+    Serial.print(accel.acceleration.x,2);   //send the X value
+    Serial.print('\t');                     //add tab separating values
+    Serial.println(accel.acceleration.y,2); //send the Y value
   }
 }
